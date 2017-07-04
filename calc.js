b8d543c8fce6290f0b01d53daf628c87e4351ce1
Julien <papao1@hotmail.fr>
2017-07-04 14:02:27 +0200

    Calculatrice avec deux versions de calculs en javascript

---
+++ calc.js
@@ -0,0 +1,19 @@
+var chiffre1 = Number (prompt("ton premier chiffre"));
+var chiffre2 = Number (prompt("ton deuxieme chiffre"));
+var sommeAdd = chiffre1 + chiffre2;
+var sommeMulti = chiffre1 * chiffre2;
+
+
+
+//alert(" ton addition donne " + sommeAdd + " et ta multiplication donne " + sommeMulti);
+
+
+// 2 eme coup
+
+var chiffre3 = Number (prompt("ton 1 premier chiffre à multiplier"));
+var chiffre4 = Number (prompt("ton multiplieur"));
+var sommeMulti2 = chiffre3 * chiffre4;
+
+alert(" ton addition donne " + sommeAdd);
+alert(" ta multiplication donne " + sommeMulti2);
+
